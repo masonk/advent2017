d42a08a030b773106b4d1a3afb293a7ec2bed9aa
Mason Kramer <mason.kramer@gmail.com>
2018-01-20 17:07:17 -0800

    Move advent18 to a mod

--- src/18/18.rs
+++ src/18/18.rs
@@ -13,8 +13,6 @@
 // rcv X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
 // jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
 // Many of the instructions can take either a register (a single letter) or a number. The value of a register is the integer it contains; the value of a number is that number.
-use std::collections::HashMap;
-use std::fmt;
 
 // pub mod assembly_instructions;
 // #[test]
@@ -26,167 +24,172 @@ use std::fmt;
 //     assert!(addr.is_ok());
 //     assert!(addr, Snd(Addr('a')));
 // }
+pub mod advent18 {
+    use std::collections::HashMap;
+    use std::fmt;
 
-type RegAdr = char;
-#[derive(Debug, Clone)]
-enum Val {
-    Addr(RegAdr),
-    Lit(i32),
-}
-#[derive(Debug, Clone)]
-enum Inst {
-    Snd(Val),
-    Set(RegAdr, Val),
-    Add(RegAdr, Val),
-    Mul(RegAdr, Val),
-    Mod(RegAdr, Val),
-    Rcv(Val),
-    Jgz(Val, Val),
-}
-
-use Inst::*;
-use Val::*;
-
-#[derive(Clone)]
-struct DuetState {
-    cursor: usize,
-    sound: Option<i32>,
-    instrs: Vec<Inst>,
-    registers: HashMap<char, i32>,
-}
-
-impl fmt::Debug for DuetState {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "DuetState {{ cursor: {}, sound: {:?}, next_instr: {:?}, registers: {:?} }}",
-            self.cursor, self.sound, self.instrs[self.cursor], self.registers
-        )
+    type RegAdr = char;
+    #[derive(Debug, Clone)]
+    enum Val {
+        Addr(RegAdr),
+        Lit(i32),
     }
-}
-impl DuetState {
-    fn new(instrs: Vec<Inst>) -> Self {
-        DuetState {
-            sound: None,
-            cursor: 0,
-            instrs,
-            registers: HashMap::new(),
+    #[derive(Debug, Clone)]
+    enum Inst {
+        Snd(Val),
+        Set(RegAdr, Val),
+        Add(RegAdr, Val),
+        Mul(RegAdr, Val),
+        Mod(RegAdr, Val),
+        Rcv(Val),
+        Jgz(Val, Val),
+    }
+
+    use self::Inst::*;
+    use self::Val::*;
+
+    #[derive(Clone)]
+    struct DuetState {
+        cursor: usize,
+        sound: Option<i32>,
+        instrs: Vec<Inst>,
+        registers: HashMap<char, i32>,
+    }
+
+    impl fmt::Debug for DuetState {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(
+                f,
+                "DuetState {{ cursor: {}, sound: {:?}, next_instr: {:?}, registers: {:?} }}",
+                self.cursor, self.sound, self.instrs[self.cursor], self.registers
+            )
         }
     }
-    fn play_until_recovery(&mut self) -> i32 {
-        loop {
-            if self.cursor > self.instrs.len() {
-                break;
+    impl DuetState {
+        fn new(instrs: Vec<Inst>) -> Self {
+            DuetState {
+                sound: None,
+                cursor: 0,
+                instrs,
+                registers: HashMap::new(),
             }
-            {
-                let inst = &self.instrs[self.cursor];
-                if let &Rcv(ref v) = inst {
-                    if self.resolve(v) != 0 {
-                        // presumably, if there's no sound at the end of the sequence, that's a code error
-                        // (Because they did not define that behavior in the problem statement)
-                        return self.sound.unwrap();
+        }
+        fn play_until_recovery(&mut self) -> i32 {
+            loop {
+                if self.cursor > self.instrs.len() {
+                    break;
+                }
+                {
+                    let inst = &self.instrs[self.cursor];
+                    if let &Rcv(ref v) = inst {
+                        if self.resolve(v) != 0 {
+                            // presumably, if there's no sound at the end of the sequence, that's a code error
+                            // (Because they did not define that behavior in the problem statement)
+                            return self.sound.unwrap();
+                        }
+                    }
+                }
+                self.do_next();
+            }
+            self.sound.unwrap()
+        }
+        fn do_next(&mut self) {
+            let inst = &self.instrs[self.cursor];
+            let mut jmp = 1;
+            println!("Before: {:?}", self);
+            match *inst {
+                Snd(ref v) => {
+                    self.sound = Some(self.resolve(v));
+                }
+                Set(addr, ref v) => {
+                    let val = self.resolve(v);
+                    self.registers.insert(addr, val);
+                }
+                Add(addr, ref v) => {
+                    let val = self.resolve(v);
+                    let mut entry = self.registers.entry(addr).or_insert(0);
+                    *entry += val;
+                }
+                Mul(addr, ref v) => {
+                    let val = self.resolve(v);
+                    let mut entry = self.registers.entry(addr).or_insert(0);
+                    *entry *= val;
+                }
+                Mod(addr, ref v) => {
+                    let val = self.resolve(v);
+                    let mut reg = self.registers.entry(addr).or_insert(0);
+                    *reg = *reg % val;
+                }
+                Rcv(ref v) => {
+                    // Don't know what "recovering" a sound is supposed to do. Maybe part 2.
+                }
+                Jgz(ref x, ref y) => {
+                    if self.resolve(x) > 0 {
+                        jmp = self.resolve(y);
                     }
                 }
             }
-            self.do_next();
+            self.cursor = ((self.cursor as i64) + (jmp as i64)) as usize;
+            println!("After: {:?}", self);
         }
-        self.sound.unwrap()
-    }
-    fn do_next(&mut self) {
-        let inst = &self.instrs[self.cursor];
-        let mut jmp = 1;
-        println!("Before: {:?}", self);
-        match *inst {
-            Snd(ref v) => {
-                self.sound = Some(self.resolve(v));
-            }
-            Set(addr, ref v) => {
-                let val = self.resolve(v);
-                self.registers.insert(addr, val);
-            }
-            Add(addr, ref v) => {
-                let val = self.resolve(v);
-                let mut entry = self.registers.entry(addr).or_insert(0);
-                *entry += val;
-            }
-            Mul(addr, ref v) => {
-                let val = self.resolve(v);
-                let mut entry = self.registers.entry(addr).or_insert(0);
-                *entry *= val;
-            }
-            Mod(addr, ref v) => {
-                let val = self.resolve(v);
-                let mut reg = self.registers.entry(addr).or_insert(0);
-                *reg = *reg % val;
-            }
-            Rcv(ref v) => {
-                // Don't know what "recovering" a sound is supposed to do. Maybe part 2.
-            }
-            Jgz(ref x, ref y) => {
-                if self.resolve(x) > 0 {
-                    jmp = self.resolve(y);
-                }
-            }
-        }
-        self.cursor = ((self.cursor as i64) + (jmp as i64)) as usize;
-        println!("After: {:?}", self);
-    }
 
-    fn resolve(&self, val: &Val) -> i32 {
-        match val {
-            &Val::Lit(v) => v,
-            &Val::Addr(ref addr) => {
-                if let Some(v) = self.registers.get(addr) {
-                    *v
-                } else {
-                    0
+        fn resolve(&self, val: &Val) -> i32 {
+            match val {
+                &Val::Lit(v) => v,
+                &Val::Addr(ref addr) => {
+                    if let Some(v) = self.registers.get(addr) {
+                        *v
+                    } else {
+                        0
+                    }
                 }
             }
         }
     }
-}
-// After each jump instruction, the program continues with the instruction to which the jump jumped. After any other instruction, the program continues with the next instruction. Continuing (or jumping) off either end of the program terminates it.
+    // After each jump instruction, the program continues with the instruction to which the jump jumped. After any other instruction, the program continues with the next instruction. Continuing (or jumping) off either end of the program terminates it.
 
-// For example:
+    // For example:
 
-// set a 1
-// add a 2
-// mul a a
-// mod a 5
-// snd a
-// set a 0
-// rcv a
-// jgz a -1
-// set a 1
-// jgz a -2
-// The first four instructions set a to 1, add 2 to it, square it, and then set it to itself modulo 5, resulting in a value of 4.
-// Then, a sound with frequency 4 (the value of a) is played.
-// After that, a is set to 0, causing the subsequent rcv and jgz instructions to both be skipped (rcv because a is 0, and jgz because a is not greater than 0).
-// Finally, a is set to 1, causing the next jgz instruction to activate, jumping back two instructions to another jump, which jumps again to the rcv, which ultimately triggers the recover operation.
-// At the time the recover operation is executed, the frequency of the last sound played is 4.
+    // set a 1
+    // add a 2
+    // mul a a
+    // mod a 5
+    // snd a
+    // set a 0
+    // rcv a
+    // jgz a -1
+    // set a 1
+    // jgz a -2
+    // The first four instructions set a to 1, add 2 to it, square it, and then set it to itself modulo 5, resulting in a value of 4.
+    // Then, a sound with frequency 4 (the value of a) is played.
+    // After that, a is set to 0, causing the subsequent rcv and jgz instructions to both be skipped (rcv because a is 0, and jgz because a is not greater than 0).
+    // Finally, a is set to 1, causing the next jgz instruction to activate, jumping back two instructions to another jump, which jumps again to the rcv, which ultimately triggers the recover operation.
+    // At the time the recover operation is executed, the frequency of the last sound played is 4.
 
-#[cfg(test)]
-mod example {
-    use DuetState;
-    use Inst::*;
-    use Val::*;
-    #[test]
-    fn example_computes() {
-        let insts = vec![
-            Set('a', Lit(1)),
-            Add('a', Lit(2)),
-            Mul('a', Addr('a')),
-            Mod('a', Lit(5)),
-            Snd(Addr('a')),
-            Set('a', Lit(0)),
-            Rcv(Addr('a')),
-            Jgz(Addr('a'), Lit(-1)),
-            Set('a', Lit(1)),
-            Jgz(Addr('a'), Lit(-2)),
-        ];
-        let mut duet = DuetState::new(insts);
-        let actual = duet.play_until_recovery();
-        assert_eq!(actual, 4);
+    #[cfg(test)]
+    mod example {
+        use super::DuetState;
+        use super::Inst::*;
+        use super::Val::*;
+        #[test]
+        fn example_computes() {
+            let insts = vec![
+                Set('a', Lit(1)),
+                Add('a', Lit(2)),
+                Mul('a', Addr('a')),
+                Mod('a', Lit(5)),
+                Snd(Addr('a')),
+                Set('a', Lit(0)),
+                Rcv(Addr('a')),
+                Jgz(Addr('a'), Lit(-1)),
+                Set('a', Lit(1)),
+                Jgz(Addr('a'), Lit(-2)),
+            ];
+            let mut duet = DuetState::new(insts);
+            let actual = duet.play_until_recovery();
+            assert_eq!(actual, 4);
+        }
     }
+    // What is the value of the recovered frequency (the value of the most recently played sound) the first time a rcv instruction is executed with a non-zero value?
+
 }
-// What is the value of the recovered frequency (the value of the most recently played sound) the first time a rcv instruction is executed with a non-zero value?
